4040f2a850313c047130b3b57ebd396581ab6b2c
Joao Filipe <florentinojoaofilipe644@gmail.com>
2021-11-07 12:41:34 -0300

    Envio da questão 02

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$pessoa = array("Nome"=>"Maria Clara","CPF"=>"087883456-18","RG"=>"2006089107117","Idade"=>"55","Altura"=>"1,75");
+
+ foreach ($pessoa as $value){
+ 	echo $value."<br>";
+ }
+ 
+?>
